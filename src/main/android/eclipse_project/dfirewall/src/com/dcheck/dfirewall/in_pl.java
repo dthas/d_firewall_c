b37cfc235e4d1188e1fa10e218fc87ac6b5b51ea
wlaq <wlaq@126.com>
2017-02-11 16:36:02 +0800

    add file write function

--- src/main/android/eclipse_project/dfirewall/src/com/dcheck/dfirewall/in_pl.java
+++ src/main/android/eclipse_project/dfirewall/src/com/dcheck/dfirewall/in_pl.java
@@ -5,10 +5,41 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button; 
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.FileOutputStream;  
+import java.io.IOException;
+import java.io.FileNotFoundException;
 
 public class in_pl extends Activity
 {
+	private EditText etip_in;
+	private EditText etport_in;
+	private String str;
+	
+	/*
+	//写数据  
+	public void writef(String fileName,String writestr) throws IOException
+	{   
+		try
+		{   
+			FileOutputStream fout =openFileOutput(fileName, MODE_PRIVATE);   
+		    byte [] bytes = writestr.getBytes();   
+		    fout.write(bytes);   
+		    fout.close();   
+		}
+		catch (FileNotFoundException e) 
+		{  
+            e.printStackTrace();  
+        }
+		catch(IOException e)
+		{   
+		    e.printStackTrace();   
+		}   
+	}  
+	*/ 
+		
 	protected void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
@@ -21,12 +52,41 @@ public class in_pl extends Activity
 		btn1 = (Button)findViewById(R.id.button_1);  
 	    btn2 = (Button)findViewById(R.id.button_2);  
 	    btn3 = (Button)findViewById(R.id.button_3);
-	
+	    
+	    etip_in 	= (EditText) findViewById(R.id.et_ip_in);
+	    etport_in 	= (EditText) findViewById(R.id.et_port_in);
+	    
 	    btn1.setOnClickListener(new Button.OnClickListener()
 	    {
 	    	public void onClick(View v)
 	    	{  
-	            
+	    		//EditText et =(EditText)findViewById(R.id.edit_text_1); 
+		    	//String str=et.getText().toString();
+		    	//writef("/data/dfirewall/txt_in_start_port.txt" ,str);
+		    	
+	    		
+	    		try
+	    		{   
+	    			//缺省保存目录：/data/data/com.dcheck.dfirewall/files
+	    			//FileOutputStream fout =openFileOutput("/data/dfirewall/txt_in_stop_port.txt", MODE_APPEND);   
+	    			FileOutputStream fout =openFileOutput("txt_in_stop_port.txt", MODE_APPEND);  
+	    			fout.write(etip_in.getText().toString().getBytes());
+		    		str	= " :";
+		    		fout.write(str.getBytes());
+		    		fout.write(etport_in.getText().toString().getBytes());
+		    		str	= "\n";
+		    		fout.write(str.getBytes());
+		    		fout.flush();
+		    		fout.close(); 
+	    		}
+	    		catch (FileNotFoundException e) 
+	    		{  
+	                e.printStackTrace();  
+	            }
+	    		catch(IOException e)
+	    		{   
+	    		    e.printStackTrace();   
+	    		} 
 	        }              
 	    });  
 	    
@@ -34,8 +94,33 @@ public class in_pl extends Activity
 	    {
 	    	public void onClick(View v)
 	    	{  
-	            
-	        }              
+	    		//EditText et =(EditText)findViewById(R.id.edit_text_1); 
+		    	//String str=et.getText().toString();
+		    	//writeFile("/data/dfirewall/txt_in_stop_port.txt" ,str);
+		    	
+		    	try
+	    		{   
+		    		//缺省保存目录：/data/data/com.dcheck.dfirewall/files
+	    			//FileOutputStream fout =openFileOutput("/data/dfirewall/txt_in_start_port.txt", MODE_APPEND);
+	    			FileOutputStream fout =openFileOutput("txt_in_start_port.txt", MODE_APPEND);
+	    			fout.write(etip_in.getText().toString().getBytes());
+		    		str	= " :";
+		    		fout.write(str.getBytes());
+		    		fout.write(etport_in.getText().toString().getBytes());
+		    		str	= "\n";
+		    		fout.write(str.getBytes());
+		    		fout.flush();
+		    		fout.close();   
+	    		}
+	    		catch (FileNotFoundException e) 
+	    		{  
+	                e.printStackTrace();  
+	            }
+	    		catch(IOException e)
+	    		{   
+	    		    e.printStackTrace();   
+	    		} 
+	        }	    	             
 	    }); 
 	    
 	    btn3.setOnClickListener(new Button.OnClickListener()
@@ -46,4 +131,6 @@ public class in_pl extends Activity
 	        }              
 	    }); 
 	}
+	
+	
 }
--- src/main/android/eclipse_project/dfirewall/src/com/dcheck/dfirewall/out_pl.java
+++ src/main/android/eclipse_project/dfirewall/src/com/dcheck/dfirewall/out_pl.java
@@ -5,14 +5,24 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button; 
+import android.widget.Button;
+import android.widget.EditText; 
+
+
+import java.io.FileOutputStream;  
+import java.io.IOException;
+import java.io.FileNotFoundException;
 
 public class out_pl extends Activity
 {
+	private EditText etip_out;
+	private EditText etport_out;
+	private String str;
+	
 	protected void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.in_pl);
+		setContentView(R.layout.out_pl);
 		
 		Button btn1;  
 		Button btn2;
@@ -21,12 +31,36 @@ public class out_pl extends Activity
 		btn1 = (Button)findViewById(R.id.button_1);  
 	    btn2 = (Button)findViewById(R.id.button_2);  
 	    btn3 = (Button)findViewById(R.id.button_3);
+	    
+	    etip_out 	= (EditText) findViewById(R.id.et_ip_out);
+	    etport_out 	= (EditText) findViewById(R.id.et_port_out);
 	
 	    btn1.setOnClickListener(new Button.OnClickListener()
 	    {
 	    	public void onClick(View v)
 	    	{  
-	            
+	    		try
+	    		{   
+	    			//缺省保存目录：/data/data/com.dcheck.dfirewall/files
+	    			//FileOutputStream fout =openFileOutput("/data/dfirewall/txt_out_stop_port.txt", MODE_APPEND);   
+	    			FileOutputStream fout =openFileOutput("txt_out_stop_port.txt", MODE_APPEND);  
+	    		    fout.write(etip_out.getText().toString().getBytes());
+	    		    str	= " :";
+	    		    fout.write(str.getBytes());
+	    		    fout.write(etport_out.getText().toString().getBytes());
+	    		    str	= "\n";
+	    		    fout.write(str.getBytes());
+	    		    fout.flush();
+	    		    fout.close();   
+	    		}
+	    		catch (FileNotFoundException e) 
+	    		{  
+	                e.printStackTrace();  
+	            }
+	    		catch(IOException e)
+	    		{   
+	    		    e.printStackTrace();   
+	    		} 
 	        }              
 	    });  
 	    
@@ -34,7 +68,28 @@ public class out_pl extends Activity
 	    {
 	    	public void onClick(View v)
 	    	{  
-	            
+	    		try
+	    		{   
+	    			//缺省保存目录：/data/data/com.dcheck.dfirewall/files
+	    			//FileOutputStream fout =openFileOutput("/data/dfirewall/txt_out_start_port.txt", MODE_APPEND);   
+	    			FileOutputStream fout =openFileOutput("txt_out_start_port.txt", MODE_APPEND);  
+	    		    fout.write(etip_out.getText().toString().getBytes());
+	    		    str	= " :";
+	    		    fout.write(str.getBytes());
+	    		    fout.write(etport_out.getText().toString().getBytes());
+	    		    str	= "\n";
+	    		    fout.write(str.getBytes());
+	    		    fout.flush();
+	    		    fout.close();   
+	    		}
+	    		catch (FileNotFoundException e) 
+	    		{  
+	                e.printStackTrace();  
+	            }
+	    		catch(IOException e)
+	    		{   
+	    		    e.printStackTrace();   
+	    		} 
 	        }              
 	    }); 
 	    
